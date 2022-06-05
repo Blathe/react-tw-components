70ea2386f10c2700b346e94693afbc673c0cefa8
Scott Peterts <Scottpeters2281@gmail.com>
2022-06-05 11:48:19 -0700

    basic routing functionality and nav sidebar added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/index'
+import About from './pages/about'
+import Contact from './pages/contact'
+
+import SideNavbar from './components/Navbar/SideNavbar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Router>
+            <div className="flex flex-row">
+            <SideNavbar />
+            <Routes>
+              <Route exact path ='/' element={<Home />} />
+              <Route exact path ='/about' element={<About />} />
+              <Route exact path ='/contact' element={<Contact />} />
+            </Routes>
+            </div>
+          </Router>
       </header>
     </div>
   );
